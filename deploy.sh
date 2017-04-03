f99c8bb923137b0ec7b41fb888ee85a637967812
Evan Savage <evan@savageinter.net>
2017-04-03 09:08:13 -0400

    Add supporter logos, remove links to T-shirt campaign

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ set -e
 set -o nounset
 
 scp *.{jpg,png,ico,html,txt,xml} savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
+scp -r supporters savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
