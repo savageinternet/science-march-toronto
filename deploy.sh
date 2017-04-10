8c80965704c53e0cd6944a3ab388bf3c75dfcac1
Evan Savage <evan@savageinter.net>
2017-04-10 12:25:37 -0400

    Add posters to website
    so that others can print them off.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -e
 set -o nounset
 
 scp *.{jpg,png,ico,html,txt,xml} savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
-scp -r supporters team savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
+scp -r supporters team posters savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
