54f0f0f12a5fe3dd1576652202c661c0bf082e04
Valkyrie Savage <valkyrie.savage@gmail.com>
2018-02-26 09:29:44 -0500

    remove dan, change tim up, shorten mohit bio, add news to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -e
 set -o nounset
 
 scp *.{jpg,png,ico,html,txt,xml} savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
-scp -r supporters team posters speakers savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
+scp -r 2017 news supporters team posters speakers savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
