9585019e7acab93b4401c35eb4aeea587a3a992e
Valkyrie Savage <valkyrie.savage@gmail.com>
2018-03-22 11:07:59 -0400

    stupid words

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-set -e
-set -o nounset
-
-scp *.{jpg,png,ico,html,txt,xml} savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
-scp -r 2017 news supporters team flyers speakers savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
+rsync * savage@oracle.savageinter.net:/var/www/html/sciencemarchto.ca
